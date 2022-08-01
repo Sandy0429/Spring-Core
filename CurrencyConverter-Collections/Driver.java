981fd5b2c8dc194416801cc38f8b51020a56d3e0
Sandy0429 <54345790+Sandy0429@users.noreply.github.com>
2022-08-01 21:34:02 +0530

    Create Driver.java

---
+++ CurrencyConverter-Collections/Driver.java
@@ -0,0 +1,18 @@
+//CurrencyConverter-XML/src/main/java/com/spring/app/Driver.java
+    package com.spring.app;
+    
+    import org.springframework.context.ApplicationContext;
+    import org.springframework.context.support.ClassPathXmlApplicationContext;
+    
+    public class Driver {
+    	
+    public static void main(String[] args)
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+         CurrencyConverter currencyConverter = context.getBean("currencyConverter", CurrencyConverter.class);
+         System.out.println(currencyConverter.getTotalCurrencyValue("5Dollar"));
+    	
+    }
+    
+   }
+   
