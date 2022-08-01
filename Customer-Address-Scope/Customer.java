f11a62a81a3b6472cd885714670bbcce06275068
Sandy0429 <54345790+Sandy0429@users.noreply.github.com>
2022-08-02 00:09:40 +0530

    Create Customer.java

---
+++ Customer-Address-Scope/Customer.java
@@ -0,0 +1,29 @@
+//Scope-Customer/src/main/java/com/spring/app/Customer.java
+    package com.spring.app;
+    
+    public class Customer {
+    
+    	private int custId;
+    	public int getCustId() {
+    		return custId;
+    	}
+    	public void setCustId(int custId) {
+    		this.custId = custId;
+    	}
+    	public String getCustName() {
+    		return custName;
+    	}
+    	public void setCustName(String custName) {
+    		this.custName = custName;
+    	}
+    	public Address getAddress() {
+    		return address;
+    	}
+    	public void setAddress(Address address) {
+    		this.address = address;
+    	}
+    	private String custName;
+    	private Address address;
+    	
+   }
+    
