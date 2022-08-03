818ae6cb5297a00045c4f121e4879fed62cecf4c
Sandy0429 <54345790+Sandy0429@users.noreply.github.com>
2022-08-03 23:02:22 +0530

    Create EmployeeDAO.java

---
+++ DBConfig-SetterBasedInjection/EmployeeDAO.java
@@ -0,0 +1,17 @@
+//DBConfig/src/main/java/com/spring/app/EmployeeDAO.java
+    package com.spring.app;
+    
+    public class EmployeeDAO
+    {
+        private DBConfig dbConfig;
+        public DBConfig getDbConfig()
+        {
+            return dbConfig;
+        }
+        public void setDbConfig(DBConfig dbConfig)
+        {
+            this.dbConfig = dbConfig;
+        }
+    	
+    }
+   
