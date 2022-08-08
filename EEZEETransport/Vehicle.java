2fd2b8867325e30f3533aa364a15e0e314da0aed
Sandy0429 <54345790+Sandy0429@users.noreply.github.com>
2022-08-08 11:45:29 +0530

    Create Vehicle.java

---
+++ EEZEETransport/Vehicle.java
@@ -0,0 +1,30 @@
+//EEZEETransport/src/main/java/com/cts/eezee/model/Vehicle.java
+    package com.cts.eezee.model;
+   
+    import java.util.Map;
+    
+    public class Vehicle {
+        private String source;
+        private Map<String, Double> destinationMap;
+    
+        public Vehicle(String source, Map<String, Double> destinationMap) {
+            this.source = source;
+            this.destinationMap = destinationMap;
+        }
+    
+       public String getSource() {
+            return source;
+        }
+    
+       public void setSource(String source) {
+            this.source = source;
+        }
+    
+        public Map<String, Double> getDestinationMap() {
+            return destinationMap;
+        }
+    
+       public void setDestinationMap(Map<String, Double> destinationMap) {
+            this.destinationMap = destinationMap;
+        }
+    }
